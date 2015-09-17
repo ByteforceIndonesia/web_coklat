6dec6857e1a894dac5881a0921b49f9697d981c4
Jonathan Hosea <jonathan.hosea@me.com>
2015-09-17 20:07:16 +0700

    added thank you html

---
+++ products/generator.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>HTML Generator</title>
+</head>
+<body>
+	<form method="post">
+    	<input type="text" name="nama" id="nama">
+        
+        <input type="text" name="harga" id="harga">
+        
+        
+    
+    
+    
+    </form>
+    
+    
+    <?php
+		
+		$name=$_POST['nama'];
+		$price=$_POST['harga'];
+		
+	
+	?>
+</body>
+</html>
